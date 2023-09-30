3b0f9a6be4f6a7ebd22444e4cda9e3415979354e
Chad Joan <chadjoan@gmail.com>
2023-09-30 18:16:51 -0400

    Bugfix for #452 , error opening config on KDE.
    This bug happens when trying to launch Fcitx Configuration
    in KDE, with the kcm-fcitx or kde-config-fcitx module already
    installed. It opens a dialog that says this:
    ```
    You're currently running KDE, but KCModule for fcitx couldn't be found, the package name of this KCModule is usually kcm-fcitx or kde-config-fcitx.
    ```
    
    It is caused by checking for the KDE version by using the
    `xprop -root KDE_SESSION_VERSION` command on a system where
    that xprop is not defined. This fix has the fcitx-configtool
    script use the `KDE_SESSION_VERSION` environment variable
    by default, and then fail back to `xprop` if it doesn't work.
    
    This fix also introduces some more precise error handling for
    the KDE/KCM detection logic, including error messages that
    should be less likely to be misleading. (The new error messages
    are English-only (untranslated) in this commit.)

--- data/script/fcitx-configtool.sh
+++ data/script/fcitx-configtool.sh
@@ -106,23 +106,97 @@ detectDE() {
 }
 
 run_kde() {
-    if xprop -root KDE_SESSION_VERSION 2>&1 | grep -q "= 5"; then
-        if (kcmshell5 --list 2>/dev/null | grep ^kcm_fcitx > /dev/null 2>&1); then
-            if [ x"$1" != x ]; then
-                exec kcmshell5 kcm_fcitx --args "$1"
-            else
-                exec kcmshell5 kcm_fcitx
-            fi
+    # The KDE_SESSION_VERSION environment variable allows us to run the correct
+    # KDE executables in this script, even if KDE's major version number
+    # is changed (ex: kcmshell4 for KDE4 vs kcmshell5 for KDE5).
+    #
+    # This KDE documentation describes the variable:
+    # https://userbase.kde.org/KDE_System_Administration/Environment_Variables
+    #
+    # But just to make sure that this variable is working the way we expect
+    # (on the current system), we can sanity check it by making sure it is
+    # defined and numeric using the below if-statement.
+    #
+    # This technique for checking whether the variable is numeric or not
+    # should be POSIX-shell compatible, and is explained here:
+    # https://stackoverflow.com/a/2704760/1261963
+    #
+    # Additionally we allow '.', '+', and '-', as future-proofing in case the
+    # version numbers ever get more complicated.
+    unset _kde_version
+    if [ -n "${KDE_SESSION_VERSION##*[!0-9.+-]*}" ]; then
+        # KDE_SESSION_VERSION variable is available, use it.
+        _kde_version="$KDE_SESSION_VERSION"
+    else
+        # Fallback: use `xprop` and hope for the best.
+        # This might not work reliably because KDE doesn't always supply
+        # the KDE_SESSION_VERSION property on the root window
+        # (this probably no longer works on newer versions of KDE).
+        # But it's better than nothing.
+
+        # On a system that supports this, the output from the `xprop` command
+        # will look something like this:
+        #     "KDE_SESSION_VERSION(CARDINAL) = 5"
+        #
+        # We use an `awk` command to split on the '= ' part of that text and
+        # retrieve the '5', and this establishes what KDE version the system
+        # is running for the current session.
+        _kde_version="$(xprop -root KDE_SESSION_VERSION 2>&1 | awk -F'[=][[:space:]]*' '{print $NF}')"
+
+        # If the above command fails, then $_kde_version will be something like:
+        #    "KDE_SESSION_VERSION:  no such atom on any window."
+        # In that case, we want to clear _kde_version to indicate that
+        # we couldn't retrieve the version number.
+        # We do this by unsetting _kde_version if it isn't numeric.
+        if [ -z "${_kde_version##*[!0-9.+-]*}" ]; then
+            unset _kde_version
         fi
     fi
 
-    # not harm to also check kde4 version on kf5
-    if (kcmshell4 --list 2>/dev/null | grep ^kcm_fcitx > /dev/null 2>&1); then
-        if [ x"$1" != x ]; then
-            exec kcmshell4 kcm_fcitx --args "$1"
-        else
-            exec kcmshell4 kcm_fcitx
+    # Error handling: if something goes wrong, we have the challenge of
+    # explaining how a wee little missing version number can ruin everything.
+    # We will do our best to provide a straightforward (layman's) explanation,
+    # followed by providing any system details that could help an expert
+    # pinpoint the root-cause more quickly.
+    unset _kde_error
+    if [ -z "$_kde_version" ]; then
+        _script_name="$0"
+        _kde_error="$(_ "You're currently running KDE, but the KDE version number could not be determined. This makes it impossible to run certain KDE commands that are needed to run the KCModule for fcitx. This could be caused by an incomplete or damaged KDE installation, a regression or bug in the $_script_name script, or some other unknown reason. Now the config file will be opened with the default text editor.")"
+
+        _xprop_output="$(xprop -root KDE_SESSION_VERSION 2>&1)"
+        if [ -z "$KDE_SESSION_VERSION" ]; then
+            _kde_error="$_kde_error $(_ "Additional details: The KDE_SESSION_VERSION environment variable is empty or undefined, and an 'xprop' command did not find a KDE_SESSION_VERSION property on any windows. The output from 'xprop -root KDE_SESSION_VERSION 2>&1' is as follows: $_xprop_output")"
+        else # if [ -z "${KDE_SESSION_VERSION##*[!0-9.+-]*}" ]; then
+            _wrong_text="$KDE_SESSION_VERSION"
+            _kde_error="$_kde_error $(_ "Additional details: The KDE_SESSION_VERSION environment variable is not a number (it is '$_wrong_text'), and an 'xprop' command did not find a KDE_SESSION_VERSION property on any windows. The output from 'xprop -root KDE_SESSION_VERSION 2>&1' is as follows: $_xprop_output")"
+        #else
+            # This branch should not be reachable.
         fi
+        return 1
+    fi
+
+    # Now that we know the KDE version number, we can use it to select which
+    # kcmshell command we will call.
+    # Thus, $_kcmshellN will with expand to
+    # `kcmshell4` for KDE4,
+    # `kcmshell5` for KDE5,
+    # or anything else for later versions of KDE.
+    _kcmshellN="kcmshell${_kde_version}"
+
+    # Search the output of `kcmshellN --list` for the `kcm_fcitx` module,
+    # then execute the module.
+    if ($_kcmshellN --list 2>/dev/null | grep ^kcm_fcitx > /dev/null 2>&1); then
+        if [ x"$1" != x ]; then
+            exec $_kcmshellN kcm_fcitx --args "$1"
+        else
+            exec $_kcmshellN kcm_fcitx
+        fi
+    else
+        # Simpler error handling for the case where `kcmshellN` simply couldn't
+        # find the module that we are looking for. This case is very likely to
+        # be caused by neglecting to install the KCModule's package using
+        # the distro's package manager.
+        _kde_error="$(_ "You're currently running KDE, but KCModule for fcitx couldn't be found, the package name of this KCModule is usually kcm-fcitx or kde-config-fcitx. Now it will open config file with default text editor.")"
     fi
 }
 
@@ -139,14 +213,11 @@ run_gtk3() {
 }
 
 run_xdg() {
-    case "$DE" in
-        kde)
-            message "$(_ "You're currently running KDE, but KCModule for fcitx couldn't be found, the package name of this KCModule is usually kcm-fcitx or kde-config-fcitx. Now it will open config file with default text editor.")"
-            ;;
-        *)
-            message "$(_ "You're currently running Fcitx with GUI, but fcitx-configtool couldn't be found, the package name is usually fcitx-config-gtk, fcitx-config-gtk3 or fcitx-configtool. Now it will open config file with default text editor.")"
-            ;;
-    esac
+	if [ -n "$_kde_error" ]; then
+		message "$_kde_error"
+	else
+		message "$(_ "You're currently running Fcitx with GUI, but fcitx-configtool couldn't be found, the package name is usually fcitx-config-gtk, fcitx-config-gtk3 or fcitx-configtool. Now it will open config file with default text editor.")"
+	fi
 
     if command="$(which xdg-open 2>/dev/null)"; then
         detect_im_addon $1
